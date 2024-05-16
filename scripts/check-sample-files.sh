8edb16e2596952ca32502034984192e0e422e201
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-05-16 13:17:53 -0700

    check samples

--- scripts/check-sample-files.sh
+++ scripts/check-sample-files.sh
@@ -3,22 +3,22 @@
 for dir in ./samples/*/
 do
   if [[ ! -f "${dir}compose.yml" ]]; then
-    echo "[ ] add compose.yml to ${dir}"
+    echo " - [ ] add compose.yml to ${dir}"
   fi
   if [[ ! -f "${dir}README.md" ]]; then
-    echo "[ ] add README.md to ${dir}"
+    echo " - [ ] add README.md to ${dir}"
   else
     if ! grep -q '^Title:' "${dir}README.md"; then
-      echo "[ ] add 'Title:' to README.md in ${dir}"
+      echo " - [ ] add 'Title:' to README.md in ${dir}"
     fi
     if ! grep -q '^Short Description:' "${dir}README.md"; then
-      echo "[ ] add 'Short Description:' to README.md in ${dir}"
+      echo " - [ ] add 'Short Description:' to README.md in ${dir}"
     fi
     if ! grep -q '^Tags:' "${dir}README.md"; then
-      echo "[ ] add 'Tags:' to README.md in ${dir}"
+      echo " - [ ] add 'Tags:' to README.md in ${dir}"
     fi
     if ! grep -q '^Languages:' "${dir}README.md"; then
-      echo "[ ] add 'Languages:' to README.md in ${dir}"
+      echo " - [ ] add 'Languages:' to README.md in ${dir}"
     fi
   fi
 done
