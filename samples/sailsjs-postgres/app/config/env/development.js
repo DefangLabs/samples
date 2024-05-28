458c7d9a21239800bb16a0e85b7c33d9e40d2c17
Christopher Yihan Jiang <48700578+Chrisyhjiang@users.noreply.github.com>
2024-05-28 16:18:48 -0700

    Working 1 object insertion for compose.yaml prod environment. Try to fix compose

--- samples/sailsjs-postgres/app/config/env/development.js
+++ samples/sailsjs-postgres/app/config/env/development.js
@@ -1,21 +1,43 @@
 module.exports = {
-    models: {
-      migrate: 'alter',
-    },
-    datastores: {
-      default: {
-        adapter: require('sails-postgresql'),
-        url: process.env.DATABASE_URL || 'postgresql://todolist_user:password@localhost:5432/todolist_db',
-      },
-    },
-    log: {
-      level: 'debug',
-    },
-    security: {
-      cors: {
-        allRoutes: true,
-        allowOrigins: ['http://localhost:1337'],  // Allow all origins for development
-      },
-      csrf: false,  // You might disable CSRF protection in development
-    },
-  };
+  port: 1337,
+
+  models: {
+    datastore: 'default',
+    migrate: 'alter'
+  },
+
+  session: {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      secure: false
+    }
+  },
+
+  sockets: {
+    onlyAllowOrigins: [
+      'http://localhost:1337',
+    ]
+  },
+
+  security: {
+    cors: {
+      allRoutes: true,
+      allowOrigins: [
+        'http://localhost:1337',
+      ],
+      allowCredentials: true,
+    }
+  },
+
+  datastores: {
+    default: {
+      adapter: 'sails-postgresql',
+      url: process.env.DATABASE_URL,
+      ssl: false
+    }
+  },
+
+  http: {
+    trustProxy: false
+  }
+};
--- samples/sailsjs-postgres/app/config/security.js
+++ samples/sailsjs-postgres/app/config/security.js
@@ -2,7 +2,8 @@ module.exports.security = {
   cors: {
     allRoutes: true,
     allowOrigins: [
-      'http://localhost:1337', // Add the appropriate origins here
+      'http://localhost:1337', // Development origin
+      'https://chrisyhjiang-app--1337.prod1.defang.dev', // Production origin
     ],
     allowCredentials: true,
   }
