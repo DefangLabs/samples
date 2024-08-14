42bd6cff1e73027e8309f24e0d7e91d50ca08d52
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-08-14 14:53:30 -0700

    switch all samples to use the defang gh action

--- scripts/check-sample-files.sh
+++ scripts/check-sample-files.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 for dir in ./samples/*/; do
+  # set variable pulumi to true if there is a Pulumi.yaml file in the directory, or there is a directory called pulumi
+  pulumi=false
+  if [[ -f "${dir}Pulumi.yaml" ]]; then
+    pulumi=true
+  elif [[ -d "${dir}pulumi" ]]; then
+    pulumi=true
+  fi
+
   if [[ ! -f "${dir}compose.yaml" ]]; then
     if find "${dir}" -name 'Pulumi.yaml' -exec false {} +; then
       echo " - [ ] add compose.yaml to ${dir}"
     fi
   fi
+
+  # Check that we have a .github/workflows/deploy.yaml file
+  if [[ ! -f "${dir}.github/workflows/deploy.yaml" && "$pulumi" == "false" ]]; then
+      echo " - [ ] add .github/workflows/deploy.yaml to ${dir} (see starter-sample)"
+  fi
+
   if [[ ! -f "${dir}README.md" ]]; then
     echo " - [ ] add README.md to ${dir}"
   else
