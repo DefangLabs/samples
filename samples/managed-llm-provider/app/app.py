7dfa822d001785d6f978e4b24d29c4a74d76d436
Jordan Stephens <jordan@stephens.io>
2025-05-22 14:09:08 -0700

    change fallback model to "default"

--- samples/managed-llm-provider/app/app.py
+++ samples/managed-llm-provider/app/app.py
@@ -20,7 +20,7 @@ default_openai_base_url = "https://api.openai.com/v1/"
 # Set the environment variables for the chat model
 LLM_URL = os.getenv("LLM_URL", default_openai_base_url) + "chat/completions"
 # Fallback to OpenAI Model if not set in environment
-MODEL_ID = os.getenv("LLM_MODEL", "gpt-4-turbo")
+MODEL_ID = os.getenv("LLM_MODEL", "default")
 
 # Get the API key for the LLM
 # For development, you can use your local API key. In production, the LLM gateway service will override the need for it.
