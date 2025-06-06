b9685fa21d28361bee9295b93715dfbbafde1ced
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2025-06-06 17:57:56 +0200

    feat: improve database readiness check using direct psycopg connection in dev script

--- samples/crew-django-redis-postgres/app/dev.sh
+++ samples/crew-django-redis-postgres/app/dev.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-# Wait for the database to be ready
-until python manage.py migrate --check; do
+# Wait for the database to be ready using psycopg 3 and dj-database-url
+until python3 -c '
+import os, sys
+import psycopg
+import dj_database_url
+
+url = os.environ.get("DATABASE_URL")
+if not url:
+    sys.exit("DATABASE_URL not set")
+
+try:
+    cfg = dj_database_url.parse(url)
+    with psycopg.connect(
+        dbname=cfg["NAME"],
+        user=cfg["USER"],
+        password=cfg["PASSWORD"],
+        host=cfg["HOST"],
+        port=cfg["PORT"] or 5432,
+    ) as conn:
+        pass  # Successful connection
+except psycopg.OperationalError:
+    sys.exit(1)
+'
+do
   echo "Waiting for database to be ready..."
   sleep 2
 done
